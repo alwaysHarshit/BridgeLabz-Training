f1d7424abef45f7881825632d3a4bf91786cb671
badmoos barua <studyharshit21@gmail.com>
2026-01-12 12:12:26 +0530

    Add recursion and tabulation approaches for LongestCommonSubsequence implementation, including test cases in main method.

--- oops-practice/leet-code-codebase/LongestCommonSubsequence.java
+++ oops-practice/leet-code-codebase/LongestCommonSubsequence.java
@@ -10,44 +10,61 @@ A common subsequence of two strings is a subsequence that is common to both stri
 import java.util.Arrays;
 
 public class LongestCommonSubsequence {
+    public static void main(String[] args) {
+        String text1 = "abcde", text2 = "ace";
 
-    //using top-down approach
-    public int longestCommonSubsequence(String text1, String text2) {
-        int i = text1.length();
-        int j = text2.length();
+        //solved by only recusrion:-> f(index1, index2): length of common sequence till index1 and index2
+        System.out.println("this is the answer from recursion: "+byRecursion(text1,text2,text1.length()-1,text2.length()-1));
 
-        int[][] dp = new int[i + 1][j + 1];
-        for (int index = 0; index < i + 1; index++) {
-            Arrays.fill(dp[index], -1);
-        }
-        return fun(text1, text2, i, j, dp);
+        //solve by Tabulation
+        System.out.println("this is the answer from Tabulation: "+byTabulation(text1,text2));
     }
 
-    int fun(String s1, String s2, int i, int j, int[][] dp) {
-        if (i == 0 || j == 0) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-        if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-            return dp[i][j] = 1 + fun(s1, s2, i - 1, j - 1, dp);
-        } else {
-            return dp[i][j] = Math.max(fun(s1, s2, i - 1, j, dp), fun(s1, s2, i, j - 1, dp));
-        }
-    }
+    private static int byTabulation(String s1, String s2) {
 
-    //using buttom-up appraoch
-    public int longestCommonSubsequence(String text1, String text2) {
-        int l1 = text1.length();
-        int l2 = text2.length();
-        int[][] dp = new int[l1 + 1][l2 + 1];
-        for (int i = 1; i <= l1; i++) {
-            for (int j = 1; j <= l2; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        // dp[i][j] -> lognest subsequece tile i and j;
+        int[][]dp=new int[s1.length()+1][s2.length()+1];
+
+        //base case: from recursion we can see that if index become -ve we return 0 but i dp we can't store -ve index
+        // so we perform shifting in which index i=0 means index -1, i=1 means index 0  and index i =k means index k-1;
+
+        for (int i = 0; i <s1.length(); i++) {
+            dp[i][0]=0;
+        }
+        for (int j = 0; j < s2.length(); j++) {
+            dp[0][j]=0;
+        }
+
+        // building dp
+        for (int i = 1; i <=s1.length() ; i++) {
+            for (int j = 1; j <=s2.length() ; j++) {
+                if (s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        return dp[l1][l2];
+
+        for (int[] ints : dp) {
+            System.out.println(Arrays.toString(ints));
+        }
+
+        // returning answer
+        return dp[s1.length()][s2.length()];
     }
 
+    private static int byRecursion(String s1, String s2, int index1, int index2) {
+
+        // base case: if index is negative that mean no element to select so length of longest subsequece is 0;
+        if(index1<0 || index2<0) return 0;
+
+        // if character matches then count 1 add call the previous elements
+        if (s1.charAt(index1)==s2.charAt(index2)){
+            return 1+byRecursion(s1, s2, index1-1, index2-1);
+        }else{
+            return Math.max(byRecursion(s1, s2, index1-1, index2), byRecursion(s1, s2, index1, index2-1));
+        }
+    }
 }
